4c5c3167a00d1bd4437f7c6da02c6e141f578a34
Abega1642 <hei.abega.2@gmail.com>
2024-07-24 20:13:10 +0300

    feat: creating <Employee> entity

---
+++ backEnd/src/main/java/com/razafindratelo/backEnd/entity/Employee.java
@@ -0,0 +1,24 @@
+package com.razafindratelo.backEnd.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "employee")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long employeeId;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+}
