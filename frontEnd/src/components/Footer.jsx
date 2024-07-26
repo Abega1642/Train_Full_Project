f38ed816e9af7d063ca9471e6d70823b418039c3
Abega1642 <hei.abega.2@gmail.com>
2024-07-26 12:56:48 +0300

    feat: add Footer & Navbar components

---
+++ frontEnd/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import '../../src/App.css'
+
+export const Footer = () => {
+  return (
+    <footer className='ftr'>
+        <span>
+            Copyright 2024 Razafindratelo Abegà
+        </span>
+    </footer>
+  )
+}
---
+++ frontEnd/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+export const Navbar = () => {
+  return (
+    <div>
+        <nav className="navbar navbar-dark bg-dark">
+            <h1 className="text-light">EmP - MaNaG</h1>
+        </nav>
+    </div>
+  )
+}
