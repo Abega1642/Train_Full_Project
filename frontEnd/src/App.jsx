7f0c0b21e2fd2db5e5acfa1adbf3c1d43d9d9770
Abega1642 <hei.abega.2@gmail.com>
2024-07-26 15:12:30 +0300

    feat: add routes in App.jsx

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,15 +1,23 @@
 import EmployeeList from './components/EmployeeList'
 import { Navbar } from './components/Navbar'
 import { Footer } from './components/Footer'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import AddEmployee from './components/AddEmployee'
 
 function App() {
 
   return (
     <>
-      <Navbar />
-      <EmployeeList />
-      <Footer />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
 
+          { /* http://localhost:3000/employees */ }
+          <Route path='/employees' element = {<EmployeeList />} />
+          <Route path='/add_employee' element = {<AddEmployee />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   )
 }
