aa0a2a5d2543fc51e5f3ac916b5e06b0407ccc36
Abega1642 <hei.abega.2@gmail.com>
2024-07-26 16:16:53 +0300

    chore: centrelized h1 in Navbar component

--- frontEnd/src/components/Navbar.jsx
+++ frontEnd/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ export const Navbar = () => {
   return (
     <div>
         <nav className="navbar navbar-dark bg-dark">
-            <h1 className="text-light">EmP - MaNaG</h1>
+            <h1 className="text-light mx-auto"><i>EmP - MaNaG</i></h1>
         </nav>
     </div>
   )
